170999e64cc0871ac094cf66a515b5f506ed855c
nguyenquocluong24 <boy.luongnguyen@gmail.com>
2025-12-04 14:07:10 +0900

    Set base path in Vite configuration
    Updated Vite configuration to include base path.

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,23 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// ĐÃ THAY ĐỔI
+const base = './'; 
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
+        server: {
+            port: 3000,
+            host: '0.0.0.0',
+        },
+        plugins: [react()],
+        base: base, // DÒNG NÀY SẼ LÀ base: './',
+        define: {
+            // ... (các cấu hình define khác)
+        },
+        resolve: {
+            // ...
         }
-      }
     };
 });
